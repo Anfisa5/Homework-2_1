2066a6034689eae9cd391aaec97970f6ede5f4a3
Anfisa Golovanova <anfisa.faa@gmail.com>
2022-10-13 01:58:26 +0700

    add file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 500;
+        int mile = 1;
+        int total = amount / 20 * mile;
+        System.out.println("Количество бонусных миль: " + total);
+
+    }
+
+}
